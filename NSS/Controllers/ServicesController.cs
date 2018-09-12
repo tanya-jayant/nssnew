39d7c1a08bb4ebf0190c35c3d8a6bfc5553b17fd
tanya-jayant <tanyajayant27@gmail.com>
2018-09-12 17:24:02 +0530

    first commit

---
+++ NSS/Controllers/ServicesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NSS.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult CyberSecurity()
+        {
+            return View();
+        }
+        public ActionResult SocialMediaMonitoring()
+        {
+            return View();
+        }
+        public ActionResult RiskAdvisory()
+        {
+            return View();
+        }
+        public ActionResult BrandProtection()
+        {
+            return View();
+        }
+        public ActionResult RiskAssessment()
+        {
+            return View();
+        }
+        public ActionResult ProtectiveServices()
+        {
+            return View();
+        }
+        public ActionResult DigitalForensics()
+        {
+            return View();
+        }
+        public ActionResult ProductDevelopment()
+        {
+            return View();
+        }
+        public ActionResult DueDiligence()
+        {
+            return View();
+        }
+    }
+}
